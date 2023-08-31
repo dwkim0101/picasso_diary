fa0902d8342e78f1b7f1ec8452088da277ea8889
Daniel Kim <dwkim0101@naver.com>
2023-08-31 18:51:04 +0900

    first commit

---
+++ android/app/src/main/kotlin/com/example/picasso_diary/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.picasso_diary
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/util/request.dart
@@ -0,0 +1,4 @@
+import 'package:openai_dalle_wrapper/openai_dalle_wrapper.dart';
+
+const apiKey = "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";
+final open_ai = OpenaiDalleWrapper(apiKey: apiKey);
