1852b231bf73b8ccbdde4eeb6b2a5f7d5d229d1a
Daniel Kim <dwkim0101@naver.com>
2024-01-10 05:48:58 +0900

    Delete lib/util/request.dart

--- lib/util/request.dart
+++
@@ -1,6 +0,0 @@
-import 'package:openai_dalle_wrapper/openai_dalle_wrapper.dart';
-
-const apiKey = "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";
-final openAi = OpenaiDalleWrapper(apiKey: apiKey);
-const apiUrl = 'https://api.openai.com/v1/completions';
-int currentPageIndex = 0;
